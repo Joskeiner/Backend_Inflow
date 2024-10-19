92546a6b265e35aa0ff93fe30c44bc3d18823c35
Agustin_Ochoa <8agustin00@gmail.com>
2024-10-19 05:45:48 -0300

    Joski puede hacer esto con ayuda de videos y una documentacion de internet probe una vez pero dio error ... yo creo que ahora si agarra... lo pruebo despues pero lo subo por si queres ver algo, tmb deje comentado tu codigo que me enseñaste, no toque nada de ahi por las dudas

--- api/product/update-product/index.js
+++ api/product/update-product/index.js
@@ -1,6 +1,9 @@
 import Product from "./models/models.js";
 
-export const handler = async (event) => {
+
+// codigo de joski no tocar 
+
+/* export const handler = async (event) => {
   let response;
   let id = event.rawQueryString;
   let product = JSON.parse(event.body);
@@ -18,4 +21,46 @@ export const handler = async (event) => {
     statusCode: 200,
     body: JSON.stringify("hello world from lambda put"),
   });
+}; */
+
+import { DynamoDBDocumentClient, UpdateCommand } from "@aws-sdk/lib-dynamodb";
+
+const client = DynamoDBDocumentClient.from(new DynamoDBClient({}));
+
+export const handler = async (event) => {
+  const { id } = event.pathParameters; 
+
+  const { name, category, amount, price } = JSON.parse(event.body);
+
+  try {
+    const result = await client.send(new UpdateCommand({
+      TableName: " ", // nombre de tu tabla
+      Key: { id }, 
+      UpdateExpression: `set name = :name, category = :category, amount = :amount, price = :price`,
+      ExpressionAttributeValues: {
+        ':name': name,
+        ':category': category,
+        ':amount': amount,
+        ':price': price
+      },
+      ReturnValues: 'ALL_NEW' // Esto te devuelve el nuevo estado del elemento
+    }));
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Producto modificado exitosamente',
+        updatedItem: result.Attributes // Devuelve el nuevo item actualizado
+      })
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: 'Error al modificar el producto',
+        error: error.message
+      })
+    };
+  }
 };
